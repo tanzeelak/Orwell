2fb1a543201333896edcba820ee380df73cfaef6
tanzeelak <tnkhan8042@gmail.com>
2018-04-28 22:00:50 -0700

    cleaned up slightly

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("hello");
-});
-
-function reformatToPs(i, item){
-    var list = item.description.split('*');
-    var html = '';
-     for(var i=0; i<list.length; i++) {
-        html += '<p>' + list[i] + '</p>';
-      }
-    // html += '</ul>';
-    item.description = html;
-}
+console.log("Big brother is always watching.");
