0a246b3337fdd0bd7b6d3abc05f38a3701a82980
tanzeelak <tnkhan8042@gmail.com>
2018-04-28 17:47:05 -0700

    mime error is gone

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 var express = require("express");
+// var path = require("path");
 var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
-var fs = require('fs');
-var logger = require("morgan");
 var hbs = require('hbs');
-//https://github.com/ucladevx/Mappening-Frontend/blob/29b2b9dde13c44a87a89d2f9ce78d0f07dcca2c4/server.js
+
 // include client-side assets and use the bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // log requests to stdout and also
-// log HTTP requests to a file in combined format
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
-app.use(logger('dev'));
-app.use(logger('combined', { stream: accessLogStream }));
+// log HTTP requests to a file in combined forma
 
 app.set('port', (process.env.PORT || 5000));
+
 app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
