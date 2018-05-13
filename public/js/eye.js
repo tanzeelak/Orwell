91743543465a69fc76dd0b6ca253930529c9fa91
tanzeelak <tnkhan8042@gmail.com>
2018-05-12 18:23:05 -0700

    one eye on the page

---
+++ public/js/eye.js
@@ -0,0 +1,82 @@
+setTimeout(function(){
+	document.getElementById('myCanvas').classList.remove('hide');
+ }, 5000);
+
+
+var renderer,
+	scene,
+	camera,
+	myCanvas = document.getElementById('myCanvas');
+
+var eye_ball_mesh, lower_lid_mesh, upper_lid_mesh;
+
+//RENDERER
+renderer = new THREE.WebGLRenderer({
+	canvas: myCanvas,
+	antialias: true
+});
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//CAMERA
+camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//SCENE
+scene = new THREE.Scene();
+
+//LIGHTS
+var light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
+
+var light2 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light2);
+
+var loader = new THREE.JSONLoader();
+var texture = new THREE.TextureLoader().load('/assets/neweye.jpg');
+var material = new THREE.MeshBasicMaterial({
+	map: texture
+})
+var material_eye_lid = new THREE.MeshBasicMaterial({
+	color: 0x303030
+});
+loader.load('/assets/eye_ball.json', eye_ball);
+loader.load('/assets/upper_lid.json', upper_lid);
+loader.load('/assets/lower_lid.json', lower_lid);
+
+function lower_lid(geometry, materials) {
+	lower_lid_mesh = new THREE.Mesh(geometry, material_eye_lid);
+	scene.add(lower_lid_mesh);
+	lower_lid_mesh.position.z = -10;
+}
+
+function upper_lid(geometry, materials) {
+	upper_lid_mesh = new THREE.Mesh(geometry, material_eye_lid);
+	scene.add(upper_lid_mesh);
+	upper_lid_mesh.position.z = -10;
+}
+
+function eye_ball(geometry, materials) {
+	eye_ball_mesh = new THREE.Mesh(geometry, material);
+	scene.add(eye_ball_mesh);
+	eye_ball_mesh.position.z = -10;
+}
+
+var upper_flag = 0;
+
+var animate = function () {
+	requestAnimationFrame(animate);
+	if (upper_lid_mesh.rotation.x < -0.04) {
+		upper_flag = 0;
+	} else if (upper_lid_mesh.rotation.x > 1) {
+		upper_flag = 1;
+	}
+	if (!upper_flag) {
+		upper_lid_mesh.rotation.x += 0.015;
+	} else {
+		upper_lid_mesh.rotation.x -= 0.015;
+	}
+	renderer.render(scene, camera);
+};
+
+animate();
