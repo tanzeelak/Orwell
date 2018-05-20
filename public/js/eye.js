52e9817e7ec28494832f24e9eb25d143e32d7196
tanzeelak <tnkhan8042@gmail.com>
2018-05-20 11:16:04 -0700

    random eyes but need to group lol

--- public/js/eye.js
+++ public/js/eye.js
@@ -1,6 +1,6 @@
-setTimeout(function() {
-  document.getElementById("myCanvas").classList.remove("hide");
-}, 2000);
+// setTimeout(function() {
+//   document.getElementById("myCanvas").classList.remove("hide");
+// }, 2000);
 
 s = document.getElementById("content").style;
 s.opacity = 1;
@@ -12,6 +12,8 @@ function fadePage() {
 var renderer,
   scenes = [],
   meshes = [],
+  randX = [],
+  randY = [],
   camera,
   myCanvas = document.getElementById("myCanvas");
 
@@ -43,7 +45,6 @@ function loadScene(callback) {
   loadTextures();
   loadLoader(callback);
   console.log("load scene");
-  // callback();
 }
 
 function loadLights() {
@@ -67,7 +68,6 @@ function loadTextures() {
 
 function loadLoader() {
   loader = new THREE.JSONLoader();
-  console.log("attempting to load 5");
   loader.load("/assets/eye_ball.json", eye_ball);
   loader.load("/assets/upper_lid.json", upper_lid);
   loader.load("/assets/lower_lid.json", lower_lid);
@@ -76,21 +76,20 @@ function loadLoader() {
 function lower_lid(geometry, materials) {
   for (var i = 0; i < 5; i++) {
     var lower_lid_mesh = new THREE.Mesh(geometry, material_eye_lid);
-    lower_lid_mesh.position.x = i;
-    lower_lid_mesh.position.y = i;
+    lower_lid_mesh.position.x = randX[i];
+    lower_lid_mesh.position.y = randY[i];
     lower_lid_mesh.position.z = -10;
-    lower_lid_mesh = "lower_lid";
+    lower_lid_mesh.name = "lower_lid";
     meshes.push(lower_lid_mesh);
     scenes[0].add(lower_lid_mesh);
   }
 }
 
 function upper_lid(geometry, materials) {
-  console.log("upper lid");
   for (var i = 0; i < 5; i++) {
     var upper_lid_mesh = new THREE.Mesh(geometry, material_eye_lid);
-    upper_lid_mesh.position.x = i;
-    upper_lid_mesh.position.y = i;
+    upper_lid_mesh.position.x = randX[i];
+    upper_lid_mesh.position.y = randY[i];
     upper_lid_mesh.position.z = -10;
     upper_lid_mesh.name = "upper_lid";
     meshes.push(upper_lid_mesh);
@@ -101,8 +100,8 @@ function upper_lid(geometry, materials) {
 function eye_ball(geometry, materials) {
   for (var i = 0; i < 5; i++) {
     var eye_ball_mesh = new THREE.Mesh(geometry, material);
-    eye_ball_mesh.position.x = i;
-    eye_ball_mesh.position.y = i;
+    eye_ball_mesh.position.x = randX[i];
+    eye_ball_mesh.position.y = randY[i];
     eye_ball_mesh.position.z = -10;
     eye_ball_mesh.name = "eye_ball";
     meshes.push(eye_ball_mesh);
@@ -113,10 +112,9 @@ function eye_ball(geometry, materials) {
 var upper_flag = 0;
 
 var animate = function() {
-  console.log("animate");
   requestAnimationFrame(animate);
   meshes.forEach(function(mesh) {
-    console.log(mesh);
+    // console.log(mesh);
     if (mesh.name == "upper_lid") {
       if (mesh.rotation.x < -0.04) {
         upper_flag = 0;
@@ -133,6 +131,17 @@ var animate = function() {
   renderer.render(scenes[0], camera);
 };
 
+function fillRandArrays(length, min, max) {
+  max = Math.abs(min) + max + 1
+  for (var i = 0; i < length; i++) {
+    randX[i] = (Math.random() * max) + min;
+    randY[i] = (Math.random() * max) + min;
+  }
+  console.log(randX);
+  console.log(randY);
+}
+
+fillRandArrays(3,-2, 2);
 fadePage();
 loadScene();
 animate();
