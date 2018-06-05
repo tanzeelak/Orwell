91874ff77c677f7a7b7f3cf822b13245bbffe1a3
tanzeelak <tnkhan8042@gmail.com>
2018-06-04 20:58:41 -0700

    first input name shows on the res

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 function showNamesList() {
+    document.getElementById("input-name").innerHTML = document.getElementById('prompt-input').value;
     var x = document.getElementById("names-list");
     if (x.classList.contains("hide")) {
         x.classList.remove("hide")
