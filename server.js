f25aa2bc24cd24354809bef50c397702bb4264db
tanzeelak <tnkhan8042@gmail.com>
2018-06-04 17:37:43 -0700

    minor changes to social media but working on mongod

--- server.js
+++ server.js
@@ -6,53 +6,65 @@ var path = require('path');
 var router = express.Router();
 var app = express();
 var mongo = require('mongodb');
+var mongoose = require('mongoose');
 var assert = require('assert');
-//var MongoClient = require('mongodb').MongoClient
-//  , assert = require('assert');
 
 var url = 'mongodb://localhost:27017/orwell'
 
+mongoose.connect('mongodb://localhost:27017/orwell'); //dbName is the database name from which you need to import the questions
+module.exports = mongoose.model('User', {
+  username: String
+})
 // include client-side assets and use the bodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // log requests to stdout and also
 // log HTTP requests to a file in combined format
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {
+  flags: 'a'
+});
 app.use(logger('dev'));
-app.use(logger('combined', { stream: accessLogStream }));
+app.use(logger('combined', {
+  stream: accessLogStream
+}));
 
 app.use(express.static(__dirname));
 
 app.set('port', (process.env.PORT || 5000));
 
-router.get("/",function(req,res){
-    res.sendFile(path.join(__dirname + '/views/' + 'social.html'));
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/' + 'social.html'));
 });
 
-router.get('/get-data', function(req, res, next){
+router.get('/get-data', function (req, res, next) {
   var resultArray = []
-  mongo.connect(url, function(err, db) {
+  mongo.connect(url, function (err, db) {
     assert.equal(null, err);
     var cursor = db.collection('user-data').find();
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      resultArray.push(doc);
-    }, function() {
-      db.close();
-      res.render('index', {items: resultArray});
-    });
+
+    // cursor.forEach(function(doc, err) {
+    //   assert.equal(null, err);
+    //   resultArray.push(doc);
+    // }, function() {
+    //   db.close();
+    //   console.log(resultArray);
+    //   res.send('/', {items: resultArray});
+    // });
   });
 });
 
-router.post('/insert', function(req, res, next){
+router.post('/insert', function (req, res, next) {
   var item = {
     username: req.body.username
   }
-  mongo.connect(url, function(err, db) {
+  mongo.connect(url, function (err, db) {
     assert.equal(null, err);
-    db.collection('user-data').insertOne(item, function(err, result){
+    db.collection('user-data').insertOne(item, function (err, result) {
       assert.equal(null, err);
+      console.log(item);
       console.log('Item inserted');
       db.close();
     });
@@ -60,24 +72,24 @@ router.post('/insert', function(req, res, next){
   res.redirect('/');
 });
 
-router.post('/update', function(req, res, next) {
+router.post('/update', function (req, res, next) {
 
 });
 
-router.post('/delete', function(req, res, next){
+router.post('/delete', function (req, res, next) {
 
 });
 
-router.get("/eye",function(req,res){
-    res.sendFile(path.join(__dirname + '/views/' + 'eye.html'));
+router.get("/eye", function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/' + 'eye.html'));
 });
 
-router.get("*",function(req,res){
+router.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + '/views/' + '404.html'));
 });
 
-app.use("/",router);
+app.use("/", router);
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
